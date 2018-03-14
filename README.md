7ca1fb8b989231d1b44f064251c011ce4fab793c
xiexianbin <xiexianbin@yovole.com>
2018-03-14 13:55:36 +0800

    init function

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 https://hub.docker.com/u/googlecontainersmirrors
 
 ref : github.com/anjia0532/gcr.io_mirror.git
+
+DOCKER_USER = xianbinxie
+DOCKER_USER_PASSWORD = 
+
--- sync.py
+++ sync.py
@@ -4,14 +4,18 @@
 # function: sync google containers registory to docker.com.
 # date 2018-3-10
 
-import ConfigParser
-from jinja2 import Template
+##
+# ENV:
+#   1. GH_TOKEN
+#   2.
+#   3.
+##
+
+import json
 import logging
-import os
-import platform
 import subprocess
 import sys
-import yaml
+import urllib2
 
 ## start log
 # create logger
@@ -35,7 +39,17 @@ logger.addHandler(oh)
 ## end log
 
 # define file path
-IMAGES = ""
+GCR_IMAGES = "https://raw.githubusercontent.com/xiexianbin/googlecontainersmirrors/sync/googlecontainersmirrors.txt"
+
+GH_TOKEN=1
+GIT_USER="xiexianbin"
+GIT_REPO="googlecontainersmirrors"
+DOCKER_REPO=GIT_REPO
+
+DOCKER_TAGS_API_URL_TEMPLATE = {
+    "docker.com": "https://registry.hub.docker.com/v1/repositories/%(repo)s/%(image)s/tags",
+    "gcr.io": "https://gcr.io/v2/%(repo)s/%(image)s/tags/list"
+}
 
 
 def _bash(command, force=False, debug=False):
@@ -45,25 +59,103 @@ def _bash(command, force=False, debug=False):
                              stderr=subprocess.PIPE)
     stdout, stderr = _subp.communicate()
     returncode = _subp.poll()
-    print "Run bash: %s, ret is %s, stderr is \n%s" % \
-        (command, returncode, stderr)
+    logger.debug("Run bash: %s, ret is %s, stderr is: %s"
+                 % (command, returncode, stderr))
 
     if not stdout and not stderr:
-        print "Run bash: %s, ret is %s" % (command, returncode)
+        logger.debug("Run bash: %s, ret is %s" % (command, returncode))
 
     if force:
         return returncode, stdout, stderr
     return returncode
 
 
+def _init_git():
+    _bash('git config user.name "xiexianbin"')
+    _bash('git config user.email "me@xiexianbin.cn"')
+
+    # clone master branch
+    _bash('git clone "https://%s@github.com/%s/%s.git"'
+          % (GH_TOKEN, GIT_USER, GIT_REPO))
+
+
+def _get_images_tags_list(domain, repo, image):
+    tags_list = []
+    url = DOCKER_TAGS_API_URL_TEMPLATE[domain] % {"repo": repo, "image": image}
+    try:
+        json_tags = json.load(urllib2.urlopen(url))
+    except urllib2.HTTPError as e:
+        logger.warn("get url: %s, except: %s"
+                    % (url, e.msg))
+        return tags_list
+    if domain == "docker.com":
+        for t in json_tags:
+            tags_list.append(t.get("name"))
+    elif domain == "gcr.io":
+        tags_list = json_tags.get("tags")
+    return tags_list
+
+
+def _sync_image(source_domain, source_repo,
+                target_domain, target_repo,
+                image, tag):
+    if target_domain == "docker.com":
+        if source_repo == "":
+            source_image = '%s/%s:%s' % (source_repo, image, tag)
+        else:
+            source_image = '%s:%s' % (image, tag)
+    else:
+        source_image = '%s/%s/%s:%s' % (source_domain, source_repo, image, tag)
+
+    if target_domain == "docker.com":
+        target_image = '%s/%s:%s' % (target_repo, image, tag)
+    else:
+        target_image = '%s/%s/%s:%s' % (target_domain, target_repo, image, tag)
+
+    logger.info("begin to sync image from %s to %s"
+                % (source_image, target_image))
+
+    # source
+    _bash('docker pull %s' % source_image)
+    # tag
+    _bash('docker tag %s %s' % (source_image, target_image))
+    # push
+    _bash('docker push %s' % target_image)
+
+    # clean the docker file
+    _bash('docker system prune -f -a')
+
+
+def _do_sync():
+    for image in urllib2.urlopen(GCR_IMAGES):
+        image = image.replace("\n", "")
+        logger.debug("Begin to sync image: [%s]" % image)
+
+        # source images tags
+        gcr_image_tags = _get_images_tags_list("gcr.io", "google_containers", image)
+        # target images tags
+        dockerhub_image_tags = _get_images_tags_list("docker.com", DOCKER_REPO, image)
+
+        for gcr_tag in gcr_image_tags:
+            if gcr_tag in dockerhub_image_tags:
+                logger.debug("image: %s, is already sync.")
+
+            # do sync
+            _sync_image("gcr.io", "google_containers",
+                        "docker.com", DOCKER_REPO,
+                        image, gcr_tag)
+
+
 def main():
     logger.info("--- Begin to sync googlecontainersmirrors ---")
 
     # 1. copy mirror
-
+    #_init_git()
+    #print
+    #print
 
     # 2. do sync
-
+    _do_sync()
 
     # 3. update
 
